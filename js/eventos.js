42d8333a4d9f81ddb2226a1d1ee067eb28b0d4a1
lorena.melloo <felipe.almeida.raposo@gmail.com>
2021-09-18 09:50:09 -0300

    Criação de constantes para elementos do js

---
+++ js/eventos.js
@@ -0,0 +1,4 @@
+const BTN_CADASTRAR = document.getElementByID('btn-cadastrar');
+const BTN_LISTAR = document.getElementByID('btn-listar');
+const BTN_SECTION_CADASTRAR = document.getElementByID('btn-section_cadastrar');
+const BTN_LISTAR = document.getElementByID('btn-listar');
