8a2d52cc94dbd8ebb645deb6dd40c68020e34ff3
lorena.melloo <felipe.almeida.raposo@gmail.com>
2021-09-18 10:14:58 -0300

    criação de elementos para ocultar e mostrar no js'

--- js/eventos.js
+++ js/eventos.js
@@ -2,3 +2,13 @@ const BTN_CADASTRAR = document.getElementByID('btn-cadastrar');
 const BTN_LISTAR = document.getElementByID('btn-listar');
 const BTN_SECTION_CADASTRAR = document.getElementByID('btn-section_cadastrar');
 const BTN_LISTAR = document.getElementByID('btn-listar');
+
+BTN-LISTAR.addEventsListener( 'Click', function () {
+  SECTION_CADASTRAR.style.display = 'nome';
+  SECTION_LISTAR.style.display = ' block';
+});
+
+BTN-CADASTRAR.addEventsListener( 'Click', function () {
+  SECTION_LISTAR.style.display = 'nome';
+  SECTION_CADASTRAR.style.display = ' block';
+});
