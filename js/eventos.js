66ab52ca9096ea25e207954d88971e0e25a34ecb
lorena.melloo <felipe.almeida.raposo@gmail.com>
2021-09-18 11:12:54 -0300

    dicionar botão

--- js/eventos.js
+++ js/eventos.js
@@ -1,14 +1,25 @@
-const BTN_CADASTRAR = document.getElementByID('btn-cadastrar');
-const BTN_LISTAR = document.getElementByID('btn-listar');
-const BTN_SECTION_CADASTRAR = document.getElementByID('btn-section_cadastrar');
-const BTN_LISTAR = document.getElementByID('btn-listar');
+const BTN_CADASTRAR = document.getElementById('btn-cadastrar');
+const BTN_LISTAR = document.getElementById('btn-listar');
+const SECTION_CADASTRAR = document.getElementById('section-cadastrar');
+const SECTION_LISTAR = document.getElementById('section-listar');
 
-BTN-LISTAR.addEventsListener( 'Click', function () {
-  SECTION_CADASTRAR.style.display = 'nome';
-  SECTION_LISTAR.style.display = ' block';
+BTN_LISTAR.addEventListener('click', function () {
+  SECTION_CADASTRAR.style.display = 'none';
+  SECTION_LISTAR.style.display = 'block';
+
+  BTN_LISTAR.classList.remove('btn-outline-dark')
+  BTN_LISTAR.classList.add('btn-dark')
+  BTN_CADASTRAR.classList.remove('btn-dark')
+  BTN_CADASTRAR.classList.add('btn-outline-dark')
 });
 
-BTN-CADASTRAR.addEventsListener( 'Click', function () {
-  SECTION_LISTAR.style.display = 'nome';
-  SECTION_CADASTRAR.style.display = ' block';
+
+BTN_CADASTRAR.addEventListener( 'click', function () {
+  SECTION_LISTAR.style.display = 'none';
+  SECTION_CADASTRAR.style.display = 'block';
+
+  BTN_LISTAR.classList.add('btn-ouline-dark');
+  BTN_LISTAR.classList.remove('btn-dark');
+  BTN_CADASTRAR.classList.add('btn-dark');
+  BTN_CADASTRAR.classList.remove('btn-outline-dark');
 });
